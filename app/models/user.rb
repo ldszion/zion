5d3f6309c9b48f77684e99d43a1a25cca5c4ea88
Marco Túlio de Ávila Santos <marco.santos@aker.com.br>
2015-06-22 12:41:50 -0300

    [modify] model User para validar presenca e uniqueness de email
    Campo email eh unico e obrigatorio para o model User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ActiveRecord::Base
   has_secure_password
+
+  validates :email,
+    presence: {presence: true, message: "Email obrigatório"},
+    uniqueness: {uniqueness: true, message: "Email já existe"}
+
+  def register_to event
+    "registrando em #{event.name}"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150617155415) do
+ActiveRecord::Schema.define(version: 20150622152838) do
+
+  create_table "events", force: :cascade do |t|
+    t.string   "name",           limit: 255
+    t.text     "description",    limit: 65535
+    t.datetime "start_datetime"
+    t.datetime "end_datetime"
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           limit: 255
@@ -19,5 +28,7 @@ ActiveRecord::Schema.define(version: 20150617155415) do
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
 
 end
