441f4d6e63086b48e32fe677290b4abe137bfc0a
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2015-06-22 20:29:54 -0300

    [add] pagina do Evento
    Foi adicionada a página que mostra o evento em detalhes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def active_item controller
+    'active' if controller == controller_name
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'users#index'
 
   resources :users
   resources :events
