ffa91e95cf26dad0e2e32f211d59ae3735ae8c1e
Marco Túlio de Ávila Santos <marco.santos@aker.com.br>
2015-06-23 12:55:48 -0300

    [add] scaffold de Region
    Foi adicionado o scaffold cru de Region.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :regions
   get 'confirmation' => 'confirmation#index', as: :confirmation
 
   # The priority is based upon order of creation: first created -> highest priority.
