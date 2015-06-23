1afd88b01093a9aa710c2ffa89695fbc3e1952aa
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2015-06-22 23:27:20 -0300

    [add] listagem de eventos
    Foi adicionada a tabela de eventos para a página de eventos.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,21 @@
 class Event < ActiveRecord::Base
+  # Retorna a data de início do evento em string
+  def start_date
+    self.start_datetime.strftime("%d/%m/%Y")
+  end
+
+  # Retorna a hora de início do evento em string
+  def start_time
+    self.start_datetime.strftime("%H:%M")
+  end
+
+  # Retorna a data de término do evento em string
+  def end_date
+    self.end_datetime.strftime("%d/%m/%Y")
+  end
+
+  # Retorna a hora de término do evento em string
+  def end_time
+    self.end_datetime.strftime("%H:%M")
+  end
 end
