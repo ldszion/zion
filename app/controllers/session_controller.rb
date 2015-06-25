5d43e1946a38054adc64117769c4b7d101349736
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2015-06-24 22:03:22 -0300

    [add] methods in session_helper
    Some helper methods were added in the session_helper for the
    log in and log out operations.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -5,15 +5,15 @@ class SessionController < ApplicationController
   def create
     user = User.find_by_email(params[:email])
     if user and user.authenticate(params[:password])
-      session[:user_id] = user.id
+      log_in user
       redirect_to root_url and return
     end
     redirect_to login_url, notice: "Usuário ou senha inválidos"
   end
 
   def destroy
-    if current_user
-      session.delete[:user_id]
+    if logged_in?
+      logout
     end
     redirect_to login_url
   end
--- app/helpers/session_helper.rb
+++ app/helpers/session_helper.rb
@@ -1,2 +1,21 @@
 module SessionHelper
+  # Logs in the given user
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  # Remove user from session
+  def logout
+    session.delete :user_id
+  end
+
+  # Returns the current logged-in user (if any).
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # Returns true if the user is logged in, false otherwise.
+  def logged_in?
+    !current_user.nil?
+  end
 end
