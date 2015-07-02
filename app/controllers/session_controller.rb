dcb3c0c65e9ad69f7a654625e6389ce5b1a736c2
Marco Túlio de Ávila Santos <marco.santos@aker.com.br>
2015-07-02 12:55:07 -0300

    [modify] session controller to accept person's params
    Added person's and user's params methods to session controller for
    security.

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -27,6 +27,28 @@ class SessionController < ApplicationController
 
   # Add user to database and signin
   def signup
-    render json: params
+    render json: person_params and return
+    person = Person.new person_params
+    person.phones.build number: params[:phone]
+    render json: person.phones
+  end
+
+  # Permit some user's fields from params
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
+
+  # Permit some person's fields from params
+  def person_params
+    params.require(:person).permit(
+      # :avatar,
+      :name,
+      :nickname,
+      :birthday,
+      :phones,
+      :ward_id,
+      :gender,
+      :agreed
+    )
   end
 end
