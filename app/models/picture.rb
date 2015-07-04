37072e3cb710abb001c3b23c155cd645490d070a
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2015-07-03 22:36:12 -0300

    [modify] Person to accept any image file type
    The person entity now accepts any image file type according to the
    regex.

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,5 @@
 class Picture < ActiveRecord::Base
   belongs_to :imageable, polymorphic: true
   has_attached_file :image, styles: {medium: "300x300>", thumb: "80x80"}, default_url: "/images/:style/default.jpg"
-  validates_attachment_content_type :image, :content_type => { :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
