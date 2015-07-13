c4dc192679786e07bfd3716140c23b541c3b695b
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2015-07-12 21:36:07 -0300

    [add] event's class comments and some fixes
    Some linter fixes were made into event's class.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,21 +1,22 @@
+# Event class
 class Event < ActiveRecord::Base
-  # Retorna a data de início do evento em string
+  # Returns the event's start date in string
   def start_date
-    self.start_datetime.strftime("%d/%m/%Y")
+    start_datetime.strftime('%d/%m/%Y')
   end
 
-  # Retorna a hora de início do evento em string
+  # Returns the event's start time in string
   def start_time
-    self.start_datetime.strftime("%H:%M")
+    start_datetime.strftime('%H:%M')
   end
 
-  # Retorna a data de término do evento em string
+  # Returns the event's end date in string
   def end_date
-    self.end_datetime.strftime("%d/%m/%Y")
+    end_datetime.strftime('%d/%m/%Y')
   end
 
-  # Retorna a hora de término do evento em string
+  # Returns the event's end time in string
   def end_time
-    self.end_datetime.strftime("%H:%M")
+    end_datetime.strftime('%H:%M')
   end
 end
