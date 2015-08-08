ec00a925a6c44ea4b3e152b5044182aa41c92bab
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2015-08-08 19:07:32 -0300

    [modify] account_helper e application_helper
    Metodo brazilian_date removido de account_helper e movido para
    application_helper porque eh utilizado em todos as outras telas e
    nao somente em account.
    
    Metodo account_thumbnail adicionado em account_helper para retornar
    a url do avatar da conta ou a imagem padrao 'avatar.jpg' para nao
    precisar implementar logica de if e else nas views.

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,5 +1,8 @@
+# Funcoes auxiliares para a Conta (account)
 module AccountHelper
-  def brazilian_date date
-    date.strftime('%d/%m/%Y')
+  # Retorna ou a url do avatar da conta ou a url padrao
+  def account_thumbnail(account)
+    return 'avatar.jpg' if account.nil?
+    account.avatar.image.url(:thumb)
   end
 end
