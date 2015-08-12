b45196ad40cd88a1a222299e8ff3b8f231eec86b
Marco Túlio de Ávila Santos <marcotulio.avila@gmail.com>
2015-08-12 00:05:08 -0300

    Refatorando estilo de codigo em events_controller e user.rb
    Alguns padroes nao foram seguidos ao escrever o events_controller
    e algumas coias em user model. Foram refatorados para satisfazer
    os padroes de estilo do Ruboto.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,54 +1,52 @@
 class EventsController < ApplicationController
-  before_action :authenticate_user, :must_have_person_if_logged_in, :must_be_active
+  before_action :authenticate_user, :must_have_person_if_logged_in,
+                :must_be_active
+  before_action :set_event, only: [:show, :destroy, :edit, :update]
 
   def index
     @events = Event.all.order(:name)
   end
 
   def show
-    @event = Event.find(params[:id])
   end
 
   def new
     time = Time.now
     @event = Event.new(start_datetime: time,
-      end_datetime: time)
+                       end_datetime: time)
   end
 
   def create
     @event = Event.new(event_params)
     convert_dates
-    if(@event.save)
-      notice = "Evento criado com sucesso"
-      redirect_to current_user, notice: notice
+    if @event.save
+      notice = 'Evento criado com sucesso'
+      redirect_to @event, notice: notice
     else
       render :new
     end
   end
 
   def destroy
-    @event = Event.find(params[:id])
     notice = nil
     alert = nil
-    if(@event.destroy!)
-      notice = "Evento excluído com sucesso!"
+    if @event.destroy!
+      notice = 'Evento excluído com sucesso!'
     else
-      alert = "Evento não pôde ser excluído, contate o administrador!"
+      alert = 'Evento não pôde ser excluído, contate o administrador!'
     end
     redirect_to :back, notice: notice, alert: alert
   end
 
   def edit
-    @event = Event.find(params[:id])
   end
 
   def update
-    @event = Event.find(params[:id])
     @event.update(event_params)
     convert_dates
-    if(@event.save)
-      notice = "Evento alterado com sucesso"
-      redirect_to current_user, notice: notice
+    if @event.save
+      notice = 'Evento alterado com sucesso'
+      redirect_to @event, notice: notice
     else
       render :edit
     end
@@ -59,21 +57,21 @@ class EventsController < ApplicationController
     user = User.find(params[:user_id])
     notice = nil
     alert = nil
-    if(Time.now < event.end_datetime)
+    if Time.now < event.end_datetime
       begin
         event.users << user
         event.save!
         user.save!
-        notice = "Inscrição realizada com sucesso!"
+        notice = 'Inscrição realizada com sucesso!'
       rescue ActiveRecord::RecordNotFound
-        alert = "Usuário ou Evento não encontrados"
+        alert = 'Usuário ou Evento não encontrados'
       rescue ActiveRecord::RecordNotUnique
-        alert = "Usuário já inscrito no evento!"
+        alert = 'Usuário já inscrito no evento!'
       rescue
-        alert = "Erro na inscrição. Contate um administrador!"
+        alert = 'Erro na inscrição. Contate um administrador!'
       end
     else
-      alert = "Inscrições não são mais permitidas neste evento!"
+      alert = 'Inscrições não são mais permitidas neste evento!'
     end
     redirect_to :back, notice: notice, alert: alert
   end
@@ -83,22 +81,25 @@ class EventsController < ApplicationController
     user = User.find(params[:user_id])
     notice = nil
     alert = nil
-    if(Time.now < event.end_datetime)
+    if Time.now < event.end_datetime
       begin
         event.users.destroy(user)
         event.save!
-        notice = "Cancelamento realizado com sucesso!"
+        notice = 'Cancelamento realizado com sucesso!'
       rescue ActiveRecord::RecordNotFound
-        alert = "Usuário ou Evento não encontrados"
+        alert = 'Usuário ou Evento não encontrados'
       rescue
-        alert = "Erro no cancelamento. Contate um administrador!"
+        alert = 'Erro no cancelamento. Contate um administrador!'
       end
     else
-      alert = "Cancelar inscrição não é mais permitido neste evento!"
+      alert = 'Cancelar inscrição não é mais permitido neste evento!'
     end
     redirect_to :back, notice: notice, alert: alert
   end
 
+  private
+
+  # Converte as datas recebidas na requisicao
   def convert_dates
     start_datetime = params[:event][:start_date] + ' ' + params[:event][:start_time]
     @event.start_datetime = start_datetime.to_datetime
@@ -106,8 +107,13 @@ class EventsController < ApplicationController
     @event.end_datetime = end_datetime.to_datetime
   end
 
-  private
+  # Define o evento a partir do id passado como parametro
+  def set_event
+    @event = Event.find(params[:id])
+  end
 
+  # Define os parametros aceitaveis para um evento. Este metodo eh
+  # complementado com o metodo convert_dates
   def event_params
     params.require(:event).permit(:name, :description)
   end
